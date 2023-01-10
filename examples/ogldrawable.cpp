22d98dfdcf3d965597bad6236da921fb7aced863
ousttrue <ousttrue@gmail.com>
2023-01-10 15:06:24 +0900

    geometry

--- examples/ogldrawable.cpp
+++ examples/ogldrawable.cpp
@@ -1,8 +1,8 @@
 #include "ogldrawable.h"
-#include "geometry.h"
 #include "xr_linear.h"
 #include <gl/glew.h>
 #include <stdlib.h>
+#include <glm/glm.hpp>
 
 static const char *VertexShaderGlsl = R"_(
     #version 410
@@ -30,6 +30,54 @@ static const char *FragmentShaderGlsl = R"_(
        FragColor = vec4(PSVertexColor, 1);
     }
     )_";
+
+namespace Geometry {
+
+struct Vertex {
+  glm::vec3 Position;
+  glm::vec3 Color;
+};
+
+constexpr glm::vec3 Red{1, 0, 0};
+constexpr glm::vec3 DarkRed{0.25f, 0, 0};
+constexpr glm::vec3 Green{0, 1, 0};
+constexpr glm::vec3 DarkGreen{0, 0.25f, 0};
+constexpr glm::vec3 Blue{0, 0, 1};
+constexpr glm::vec3 DarkBlue{0, 0, 0.25f};
+
+// Vertices for a 1x1x1 meter cube. (Left/Right, Top/Bottom, Front/Back)
+constexpr glm::vec3 LBB{-0.5f, -0.5f, -0.5f};
+constexpr glm::vec3 LBF{-0.5f, -0.5f, 0.5f};
+constexpr glm::vec3 LTB{-0.5f, 0.5f, -0.5f};
+constexpr glm::vec3 LTF{-0.5f, 0.5f, 0.5f};
+constexpr glm::vec3 RBB{0.5f, -0.5f, -0.5f};
+constexpr glm::vec3 RBF{0.5f, -0.5f, 0.5f};
+constexpr glm::vec3 RTB{0.5f, 0.5f, -0.5f};
+constexpr glm::vec3 RTF{0.5f, 0.5f, 0.5f};
+
+#define CUBE_SIDE(V1, V2, V3, V4, V5, V6, COLOR)                               \
+  {V1, COLOR}, {V2, COLOR}, {V3, COLOR}, {V4, COLOR}, {V5, COLOR}, {V6, COLOR},
+
+constexpr Vertex c_cubeVertices[] = {
+    CUBE_SIDE(LTB, LBF, LBB, LTB, LTF, LBF, DarkRed)   // -X
+    CUBE_SIDE(RTB, RBB, RBF, RTB, RBF, RTF, Red)       // +X
+    CUBE_SIDE(LBB, LBF, RBF, LBB, RBF, RBB, DarkGreen) // -Y
+    CUBE_SIDE(LTB, RTB, RTF, LTB, RTF, LTF, Green)     // +Y
+    CUBE_SIDE(LBB, RBB, RTB, LBB, RTB, LTB, DarkBlue)  // -Z
+    CUBE_SIDE(LBF, LTF, RTF, LBF, RTF, RBF, Blue)      // +Z
+};
+
+// Winding order is clockwise. Each side uses a different color.
+constexpr unsigned short c_cubeIndices[] = {
+    0,  1,  2,  3,  4,  5,  // -X
+    6,  7,  8,  9,  10, 11, // +X
+    12, 13, 14, 15, 16, 17, // -Y
+    18, 19, 20, 21, 22, 23, // +Y
+    24, 25, 26, 27, 28, 29, // -Z
+    30, 31, 32, 33, 34, 35, // +Z
+};
+
+} // namespace Geometry
 
 OglDrawable::OglDrawable() {}
 
