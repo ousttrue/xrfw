0316b71caf3043a7ef224f56999cda9112a5776f
ousttrue <ousttrue@gmail.com>
2023-01-14 16:51:05 +0900

    microsoft/OpenXR-MixedReality

--- README.md
+++ README.md
@@ -7,12 +7,22 @@ PC は `meson` で、 Android は gradle から使えるように `cmake` でビ
 
 ## Feature
 
-* [ ] initialize OpenGL(Windows) `XR_TYPE_GRAPHICS_REQUIREMENTS_OPENGL_KHR`
-* [ ] initialize OpenGLES(Android) `XR_TYPE_GRAPHICS_REQUIREMENTS_OPENGL_ES_KHR`
+|platform|graphics|||
+|-|-|-|-|
+|Windows|D3D11|||
+|Windows|OpenGL|XR_TYPE_GRAPHICS_REQUIREMENTS_OPENGL_KHR|✅|
+|Windows|Vulkan|||
+|Android|OpenGL ES| XR_TYPE_GRAPHICS_REQUIREMENTS_OPENGL_ES_KHR|✅|
+|Android|Vulkan|||
 
-## 参考
+## openxr_loader
 
 - https://github.com/KhronosGroup/OpenXR-SDK-Source
   - xrfw_sample
 - https://developer.oculus.com/documentation/native/android/mobile-intro/
   - XrApp
+
+## samples port from
+
+- https://github.com/terryky/android_openxr_gles
+- https://github.com/microsoft/OpenXR-MixedReality/tree/main/samples/BasicXrApp
---
+++ app_BasicXrApp/pch.h
@@ -0,0 +1,37 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#pragma once
+
+#include <memory>
+#include <string>
+#include <vector>
+#include <thread>
+#include <chrono>
+#include <atomic>
+#include <array>
+#include <map>
+#include <list>
+#include <unordered_map>
+#include <algorithm>
+#include <assert.h>
+
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#include <windows.h>
+
+#include <d3d11.h>
+
+#define XR_USE_PLATFORM_WIN32
+#define XR_USE_GRAPHICS_API_D3D11
+#include <openxr/openxr.h>
+#include <openxr/openxr_platform.h>
+#include <openxr/openxr_reflection.h>
+
+#define ENABLE_GLOBAL_XR_DISPATCH_TABLE
+#include <XrUtility/XrDispatchTable.h>
+#include <XrUtility/XrError.h>
+#include <XrUtility/XrHandle.h>
+#include <XrUtility/XrMath.h>
+#include <XrUtility/XrString.h>
+#include <winrt/base.h> // winrt::com_ptr
